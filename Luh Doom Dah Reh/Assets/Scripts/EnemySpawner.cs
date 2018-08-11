5e1d4339d3ecfa646915b4e4f46d7b662570d662
Benjamin Hii Kheng Soon <tp038170@mail.apu.edu.my>
2018-08-11 19:27:04 +0800

    - Updated EnemySpawner Script

--- Luh Doom Dah Reh/Assets/Scripts/EnemySpawner.cs
+++ Luh Doom Dah Reh/Assets/Scripts/EnemySpawner.cs
@@ -4,11 +4,41 @@ using UnityEngine;
 
 public class EnemySpawner : MonoBehaviour {
 
+    [SerializeField] private bool isStationary = true;
+    [SerializeField] private int enemyTypeInt = 0;
+    [SerializeField] private float minSpawnRate = 10.1f;
+    [SerializeField] private float MaxSpawnRate = 20.1f;
+
     [SerializeField] private int maxNumOfEnemies;
 
-    private void Update() {
-        if (EnemyAI.numEnemiesToSpawn > 0 && EnemyAI.numEnemiesLeft < maxNumOfEnemies) {
+    [SerializeField] private List<GameObject> enemyPrefab = new List<GameObject>();
 
+    private bool canSpawn = false;
+
+    private float time = 0;
+
+    private void Update() {
+        time += Time.deltaTime;
+
+        if ((!canSpawn || enemyTypeInt >= enemyPrefab.Count) && time < Random.Range(minSpawnRate, MaxSpawnRate)) {
+            return;
+        }
+
+        if (EnemyAI.numEnemiesToSpawn > 0 && EnemyAI.numEnemiesLeft < maxNumOfEnemies) {
+            var enemy = Instantiate(enemyPrefab[enemyTypeInt], transform.position, transform.rotation);
+            time = 0;
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision) {
+        if (isStationary && collision.gameObject.tag == "Player") {
+            canSpawn = false;
+        }
+    }
+
+    private void OnCollisionExit(Collision collision) {
+        if (isStationary && collision.gameObject.tag == "Player") {
+            canSpawn = true;
         }
     }
 }
