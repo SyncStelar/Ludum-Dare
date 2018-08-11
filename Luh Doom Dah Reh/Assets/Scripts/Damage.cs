ebbe2fb81598f1930f8d6835596e8cbe1e7bea40
PuckKishen <33612537+PuckKishen@users.noreply.github.com>
2018-08-11 16:27:56 +0800

    HP and Damage

---
+++ Luh Doom Dah Reh/Assets/Scripts/Damage.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Damage : MonoBehaviour {
+    public float damage = 100f;
+
+	void OnTriggerEnter(Collider col)
+    {
+
+        if (col.tag == "Player")
+        {
+            PlayerHP player = col.GetComponent<PlayerHP>();
+            player.TakenDamage(damage);
+        }
+    }
+}
---
+++ Luh Doom Dah Reh/Assets/Scripts/PlayerHP.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerHP : MonoBehaviour {
+    public float health = 100f;
+//    public AudioSource TargetHurt;
+
+    // Use this for initialization
+    public void TakenDamage(float amount)
+    {
+//        TargetHurt.Play();
+        health -= amount;
+        if (health <= 0f)
+        {
+            Eliminate();
+        }
+    }
+
+    // Update is called once per frame
+    void Eliminate()
+    {
+        Destroy(gameObject);
+    }
+}
