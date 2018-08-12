dc21ad3b1340b3dde2ee32c2dda8ac97b1b0af90
roudrosk <33644034+roudrosk@users.noreply.github.com>
2018-08-12 14:58:55 +0800

    splody shieet

---
+++ Luh Doom Dah Reh/Assets/Scripts/ProjectileScript.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProjectileScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        Destroy(gameObject, 4f);
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Enemy")
+        {
+            collision.gameObject.GetComponent<EnemyAI>().TakeDamage(15f);
+            Destroy(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+            Debug.Log("Hit Something");
+        }
+
+        
+    }
+}
---
+++ Luh Doom Dah Reh/Assets/Scripts/RocketScript.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RocketScript : MonoBehaviour {
+
+    [SerializeField]
+    public float Damage = 40f;
+    public GameObject explosion;
+
+	// Use this for initialization
+	void Start () {
+        Destroy(gameObject, 6f);
+
+
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Enemy")
+        {
+            collision.gameObject.GetComponent<EnemyAI>().TakeDamage(Damage);
+            Explode();
+        }
+        else
+        {
+            if (collision.gameObject.tag != "Player")
+            {
+                Explode();
+                //Destroy(collision.gameObject);
+                Debug.Log("Hit Something");
+
+            }
+            
+        }
+
+
+    }
+
+    void Explode()
+    {
+        var splodin = Instantiate(explosion, transform.position, Quaternion.identity);
+
+        Destroy(gameObject);
+        Destroy(splodin, 1.5f);
+
+    }
+}
