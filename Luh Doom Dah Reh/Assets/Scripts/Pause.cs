324441bd6930c1c1330ec76b3b7853e73fb260f2
PuckKishen <33612537+PuckKishen@users.noreply.github.com>
2018-08-12 14:31:21 +0800

    uhhhhhhhhhhhh

---
+++ Luh Doom Dah Reh/Assets/Scripts/Pause.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Pause : MonoBehaviour {
+    public Canvas UICanvas;
+    public Canvas PauseCanvas;
+
+
+	// Use this for initialization
+	void Start () {
+        UICanvas.enabled = true;
+        PauseCanvas.enabled = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if(Input.GetKeyDown(KeyCode.Escape))
+        {
+            PauseCanvas.enabled = true;
+            UICanvas.enabled = false;
+        }
+	}
+}
--- Luh Doom Dah Reh/Assets/Scripts/PlayerHP.cs
+++ Luh Doom Dah Reh/Assets/Scripts/PlayerHP.cs
@@ -1,20 +1,24 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerHP : MonoBehaviour {
     public float health = 100f;
+    public Image HP;
 //    public AudioSource TargetHurt;
 
     // Use this for initialization
     public void TakenDamage(float amount)
     {
+        
 //        TargetHurt.Play();
         health -= amount;
         if (health <= 0f)
         {
             Eliminate();
         }
+        HP.fillAmount = health;
     }
 
     // Update is called once per frame
