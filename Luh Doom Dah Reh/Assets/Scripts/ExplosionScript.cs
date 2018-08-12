91d467eace45ddfa8ba7eb05d1e0bc09eec177f5
roudrosk <33644034+roudrosk@users.noreply.github.com>
2018-08-12 15:51:49 +0800

    new rpg

---
+++ Luh Doom Dah Reh/Assets/Scripts/ExplosionScript.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExplosionScript : MonoBehaviour {
+
+    SphereCollider Item;
+
+	// Use this for initialization
+	void Start () {
+        Item = gameObject.GetComponent<SphereCollider>();
+        ScanForItems(Item);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void ScanForItems(SphereCollider Item)
+    {
+        Vector3 center = Item.transform.position + Item.center;
+        float radius = Item.radius;
+
+        Collider[] allOverlappingColliders = Physics.OverlapSphere(center, radius);
+    }
+}
