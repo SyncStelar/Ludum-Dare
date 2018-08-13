97af833ee9069b5e9cfc73519239494fc69deb32
Benjamin Hii Kheng Soon <tp038170@mail.apu.edu.my>
2018-08-13 23:51:20 +0800

    - Added KillPlayer script -Made Volcano a prefab

---
+++ Luh Doom Dah Reh/Assets/Scripts/KillPlayer.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KillPlayer : MonoBehaviour {
+    private void OnCollisionEnter(Collision collision) {
+        if (collision.gameObject.tag == "Player") {
+            collision.gameObject.GetComponent<PlayerHP>().TakenDamage(collision.gameObject.GetComponent<PlayerHP>().health);
+        }
+    }
+}
